1a604e016342ee9b0cf49c8f4f1b6a66fb285ccc
Sheep <loveandsheep@Karakul.local>
2012-08-24 10:12:06 +0900

    add_invert_affin

---
+++ pers_rectangle_invert.h
@@ -0,0 +1,17 @@
+//
+//  pers_rectangle.h
+//  ofxMpplrExample
+//
+//  Created by 洋紀 加治 on 12/04/23.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+#include "ofMain.h"
+#include "ofxOpenCv.h"
+
+class pers_rectangle{
+public:
+    ofRectangle src;
+    ofPoint srcp[4];
+    ofPoint pts[4];
+    void setMatrix(int width,int height);
+};
